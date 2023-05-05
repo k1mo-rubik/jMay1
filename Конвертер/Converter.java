14817c02502b2a842d53654ba8511e0f214ff68e
Pympikson <71492672+Pympikson@users.noreply.github.com>
2023-05-05 20:06:20 +0300

    выполнено

--- Конвертер/Converter.java
+++ Конвертер/Converter.java
@@ -1,38 +1,52 @@
 package Homework_8_and_9.Converter;
 
-import java.io.BufferedWriter;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.file.*;
+import java.util.Arrays;
 import java.util.List;
 
 public class Converter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         if (args.length != 4) {
             System.out.println("Неверное количество аргументов. " +
                     "Ожидалось 4: -путь_к_иcходному_файлу -путь_к_выходному_файлу -кодировка_исходного_файла -кодировка_выходного_файла");
             return;
         }
 
-        String filename = args[0];
-        String filenameOut = args[1];
-        String fileCharset = args[2];
-        String fileCharsetOut = args[3];
+        String sourceFile = args[0];
+        String targetFile = args[1];
+        String sourceEncoding = args[2];
+        String targetEncoding = args[3];
 
-        try {
-            Path file = Paths.get(filename);
-            List<String> lines = Files.readAllLines(file, Charset.forName(fileCharset));
+//        String sourceFile = "utf8.txt";
+//        String targetFile = "w1251.txt";
+//
+//        String sourceEncoding = "Windows-1251";
+//        String targetEncoding = "UTF-8";
+        // Создадим FileInputStream для исходного файла
+        FileInputStream fis = new FileInputStream(sourceFile);
+        InputStreamReader isr = new InputStreamReader(fis, sourceEncoding);
 
-            Path fileOut = Paths.get(filenameOut);
-            try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(fileOut.toUri()),
-                    Charset.forName(fileCharsetOut), StandardOpenOption.CREATE_NEW)) {
-                for (String line : lines) {
-                    writer.append(line);
-                }
-            }
+        //Создадим FileOutputStream для целевого файла
+        FileOutputStream fos = new FileOutputStream(targetFile);
+        OutputStreamWriter osw = new OutputStreamWriter(fos, targetEncoding);
+
+        //Считываем из входного потока и записываем в выходной поток
+        char[] buffer = new char[1024];
+        int length;
+        // записываем в буфер пока не дочитаем файл до конца
+        while ((length = isr.read(buffer)) != -1) {
+            osw.write(buffer, 0, length);
         }
-        catch (Exception ex)
-        {
-            System.out.println(ex.getStackTrace());
-        }
+    // закрываем потоки
+        isr.close();
+        fis.close();
+        osw.close();
+        fos.close();
+
+        System.out.println("Конвертация пройдена");
+
+
     }
 }
